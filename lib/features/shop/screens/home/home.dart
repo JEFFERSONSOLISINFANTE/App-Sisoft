09f2c3d9d816726c48a1915a659174ce3069d9e6
JEFFERSONSOLISINFANTE <1374414@senati.pe>
2024-06-28 17:05:27 -0500

    Barra de busqueda y categorias

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,25 +1,65 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:newapp/common/widgets.login_signup/appbar/appbar.dart';
 import 'package:newapp/common/widgets.login_signup/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:newapp/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:newapp/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:newapp/utils/constants/colors.dart';
+import 'package:newapp/utils/constants/image_strings.dart';
+import 'package:newapp/utils/constants/sizes.dart';
+import 'package:newapp/utils/device/device_utility.dart';
+import 'package:newapp/utils/helpers/helper_functions.dart';
 
 import '../../../../common/widgets.login_signup/custom_shapes/containers/circular_container.dart';
 import '../../../../common/widgets.login_signup/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets.login_signup/custom_shapes/containers/search_container.dart';
 import '../../../../common/widgets.login_signup/custom_shapes/curved_edges/curved_edges_widget.dart';
+import '../../../../common/widgets.login_signup/image_text_widgets/vertical_image_text.dart';
+import '../../../../common/widgets.login_signup/products.cart/cart_menu_icon.dart';
+import '../../../../common/widgets.login_signup/texts/section_heading.dart';
+import '../../../../utils/constants/text_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-              child: Container()
-            ),
+                child: Column(
+              children: [
+                ///Barra Principal
+                THomeAppBar(),
+                SizedBox(height: TSizes.spaceBtwSections),
+
+                ///Barra de Busqueda
+                TSearchContainer(text: 'Buscar'),
+                SizedBox(height: TSizes.spaceBtwSections),
+
+                ///Categorias
+                Padding(
+                  padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                  child: Column(
+                    children: [
+                      ///Titulo
+                      TSectionHeading(
+                          title: 'Categorias Populares',
+                          showActionButton: false,
+                          textColor: TColors.white),
+                      SizedBox(height: TSizes.spaceBtwItems),
+
+                      ///Categorias
+                      THomeCategories(),
+                    ],
+                  ),
+                )
+              ],
+            )),
           ],
         ),
       ),
--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,22 +1,32 @@
 /// This class contains all the App Images in String formats.
 class TImages {
 
-  // App Logos
+  // App Logo
     static const String darkAppLogo = "assets/logos/t-store-splash-logo-black.png";
     static const String lightAppLogo = "assets/logos/t-store-splash-logo-white.png";
 
-  // Social Logos
+  // Redes sociales
     static const String google = "assets/logos/google.png";
     static const String facebook = "assets/logos/facebook.png";
 
-  // -- OnBoarding Images
+  // -- Imagenes de Inicio
     static const String onBoardingImage1 = "assets/images/on_boarding_images/giphy.gif";
     static const String onBoardingImage2 = "assets/images/on_boarding_images/giphy2.gif";
     static const String onBoardingImage3 = "assets/images/on_boarding_images/giphy3.gif";
 
 
-    // -- Animations Images
+    // -- Imagenes gif
     static const String verifyIllustration = "assets/images/animations/emailverify.gif";
+
+    // -- Iconos
+    static const String pcIcon = "assets/icons/categories/pc.png";
+    static const String laptopIcon = "assets/icons/categories/laptop.png";
+    static const String tintaIcon = "assets/icons/categories/tinta.png";
+    static const String camaraIcon = "assets/icons/categories/camara.png";
+    static const String mantenimientoIcon = "assets/icons/categories/mantenimiento.png";
+    static const String impresoraIcon = "assets/icons/categories/impresora.png";
+
+
 
 
 }
